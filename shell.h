578d50107b05608b7046dde8f7a545024efd81c8
Avraham sabann <66841899+albertmonamor@users.noreply.github.com>
2023-09-19 12:08:36 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -9,5 +9,10 @@ using namespace std;
 
 // run commmand by using cmd.exe file, using pipe to write & read
 // not support multiply commands, cant use && to chains commands
+// ---
+// param cmd: יריץ cmd.exe הפקודה שהתהליך 
+// param lout: length output 
+//
+// ---
 char* run_pshell(const string& cmd, size_t&lout);
 
